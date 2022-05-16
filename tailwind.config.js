2e09d8f1757106bb7fb7ee3f9e099ba12a0ae94e
Elivelton Santos <samsepi0l.dev@gmail.com>
2022-05-16 01:38:52 -0300

    create project and install depences

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+      colors: {
+        brand: {
+          500: '#0D114F'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
